74dfcde557efa4b15efaa29d66f542080f0b7168
IrinaOleynik <irinaoleynik615@gmail.com>
2024-06-22 13:51:41 +0300

    Annotation Config added

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,41 @@
+package repository;
+
+import exception.NotFoundException;
+import model.Post;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class PostRepository {
+    private AtomicInteger newId = new AtomicInteger(0);
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.of(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(newId.getAndIncrement());
+            posts.put(post.getId(), post);
+        } else if (!posts.containsKey(post.getId())) {
+            posts.put(post.getId(), post);
+        } else {
+            throw new NotFoundException("Post not found");
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.containsKey(id)) {
+            posts.remove(id);
+        } else {
+            throw new NotFoundException("Post with id " + id + " not found");
+        }
+    }
+}
---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import controller.PostController;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+
+    private final String PATH = "/api/posts";
+    private final String GET = "GET";
+    private final String POST = "POST";
+    private final String DELETE = "DELETE";
+    final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+            "controller", "repository", "service");
+    final PostController controller = context.getBean(PostController.class);
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            if (method.equals(GET) && path.equals(PATH)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches("/api/posts/\\d+")) {
+                controller.getById(getId(path), resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(PATH)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches("/api/posts/\\d+")) {
+                controller.removeById(getId(path), resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+    protected long getId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
+}
